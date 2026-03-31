dc33dc473c627f173f26fb0918b8bb262509a0ca
RifqyFauzan9 <r1fqyf4uz4n@gmail.com>
2026-03-31 16:51:04 +0700

    menambahkan animasi hover ke navbar

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
-// Entry point
-console.log('Sriwijaya Air app is running');
+const sections = document.querySelectorAll('section')
+const navLinks = document.querySelectorAll('.nav-link')
+
+const options = { threshold: 0.6 }
+
+const callback = (entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            const activeId = entry.target.getAttribute('id')
+
+            navLinks.forEach((link) => {
+                link.classList.add("text-gray-400")
+
+                if (link.getAttribute("href") === `#${activeId}`) {
+                    link.classList.add("text-[#313161]")
+                    link.classList.remove("text-gray-400")
+                }
+            })
+        }
+    })
+}
+
+const observer = new IntersectionObserver(callback, options)
+sections.forEach(section => observer.observe(section))
